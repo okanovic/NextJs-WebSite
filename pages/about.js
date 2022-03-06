aa7bb1ddaa40659f4556c53347e29b3553f25e63
Okan TOPER <okan.toper94@gmail.com>
2022-03-06 20:19:18 +0300

    module eklendi

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,13 @@
 import Layout from "../components/layout";
 import Head from "next/head";
+import styles from './about.module.css'
 export default function AboutPage() {
   return (
     <Layout>
       <Head>
         <title>About Me</title>
       </Head>
-      <h1>Welcome About Page </h1>
+      <h1 className={styles.title}>Welcome About Page </h1>
     </Layout>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,13 @@ function HomePage() {
   return (
     <Layout>
       <h1> Welcome to Next.js!</h1>
+      <style jsx>{`
+        h1 {
+          background-color: rgba(219, 35, 219, 0.308);
+          padding: 10px;
+          border-radius:5px
+        }
+      `}</style>
     </Layout>
   );
 }
