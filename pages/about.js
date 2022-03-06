9f24cf8fa0df6c7c9e14ed042dfa0f461be27d64
Okan TOPER <okan.toper94@gmail.com>
2022-03-06 18:37:29 +0300

    pages created

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,8 @@
 import Layout from '../components/layout'
-export function AboutPage(){
+export default function AboutPage(){
     return (
         <Layout>
              <h1>Welcome About Page </h1>
         </Layout>
     )
 }
-
-export default AboutPage
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 import Layout from '../components/layout'
 
-export function HomePage() {
+export default function HomePage() {
     return (
         <Layout>
             <h1> Welcome to Next.js!</h1>
         </Layout>
     )
 }
-
-export default HomePage
